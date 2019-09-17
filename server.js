2783e6adcd8183d7016f7227ccdef3b5bd618fd2
smartbrainproject <55325125+smartbrainproject@users.noreply.github.com>
2019-09-17 21:32:37 +0300

    asasda

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "123456",
-    database: "work-list"
+    connectString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 db.select("*")
